23b86a02076216dc640bb731a655b9d6360ee9ad
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2023-07-29 08:32:15 -0400

    Update v4.0 indexMain.java
    added edit_distance and dictionary

--- ACC Final Project/Acc Project/src/project/indexMain.java
+++ ACC Final Project/Acc Project/src/project/indexMain.java
@@ -1,6 +1,11 @@
 package project;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class indexMain {
@@ -21,6 +26,41 @@ public class indexMain {
     	
     }
 
+public static void spellCheck(String pattern) throws IOException {
+		String filename="dictionary.txt";
+		File file = new File(filename);
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			ArrayList<String> dictonarywords = new ArrayList<String>();
+			String str=null;
+			while((str= reader.readLine())!=null)
+			{
+				dictonarywords.add(str);
+			}
+			
+			int ed,MinimumDistance=10, secondMinimumDist=10;
+			int sugWordOne=0;
+			for(int i = 0; i<dictonarywords.size();i++){
+				String dw=dictonarywords.get(i);
+				ed = EditDistance.minDistance(dw, pattern);
+				if(ed<secondMinimumDist) {
+					if(ed<MinimumDistance) {
+						MinimumDistance=ed;
+						sugWordOne=i;
+					}
+				
+				}
+			}
+			System.out.println(" Entered Keyword is not present, Instead search for "+dictonarywords.get(sugWordOne));
+		}	
+	}
+
+    public static void suggestions(String pattern) {
+		try {
+			spellCheck(pattern);
+		}catch (Exception e) {
+			System.out.println("Exception:" + e);
+		}
+	}
 
     public static void main(String[] args) throws IOException{
         runWebcrawl();
@@ -35,10 +75,15 @@ public class indexMain {
         
         if(l == null){
             System.out.println("The word "+word+" is not found");
+            System.out.println(" Word suggestions :");
+			Dict.createDictionary();
+            suggestions(word);
         }
         else{
             System.out.println("The maximum number of occurences of the word: "+word+" is in "+l);
             KMPsearchfile(l,word);}
+        
+        
     }
     
 }
